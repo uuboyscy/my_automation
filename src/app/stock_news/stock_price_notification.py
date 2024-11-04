a92f18398d940524b7ce590016d4c35617496940
uuboyscy <aegis12321@gmail.com>
2024-11-04 20:31:44 +0800

    feat: :sparkles: Stock price notification

--- src/app/stock_news/stock_price_notification.py
+++ src/app/stock_news/stock_price_notification.py
@@ -37,28 +37,45 @@ def get_stock_info(symbol):
     # Fetch stock price in the past 3 months
     hist_3mo = stock.history(period="3mo")
     high_price_3mo = hist_3mo["Close"].max()
-    mean_price_3mo = hist_3mo["Close"].mean()  # noqa: F841
+    mean_price_3mo = hist_3mo["Close"].mean()
 
     # Fetch stock price in the past 6 months
     hist_6mo = stock.history(period="6mo")
-    high_price_6mo = hist_6mo["Close"].max()  # noqa: F841
-    mean_price_6mo = hist_6mo["Close"].mean()  # noqa: F841
+    high_price_6mo = hist_6mo["Close"].max()
+    mean_price_6mo = hist_6mo["Close"].mean()
 
     # Fetch stock price in the past 1 year
-    hist_1y = stock.history(period="1y")
-    high_price_1y = hist_1y["Close"].max()  # noqa: F841
-    mean_price_1y = hist_1y["Close"].mean()  # noqa: F841
+    hist_1yr = stock.history(period="1y")
+    high_price_1yr = hist_1yr["Close"].max()
+    mean_price_1yr = hist_1yr["Close"].mean()
 
-    percentage = (current_price / high_price_3mo - 1) * 100
-    return current_price, percentage
+    relative_high_percentage_3mo = (current_price / high_price_3mo - 1) * 100
+    relative_high_percentage_6mo = (current_price / high_price_6mo - 1) * 100
+    relative_high_percentage_1yr = (current_price / high_price_1yr - 1) * 100
+    relative_mean_percentage_3mo = (current_price / mean_price_3mo - 1) * 100
+    relative_mean_percentage_6mo = (current_price / mean_price_6mo - 1) * 100
+    relative_mean_percentage_1yr = (current_price / mean_price_1yr - 1) * 100
+
+    return {
+        "current_price": current_price,
+        "relative_high_percentage_3mo": relative_high_percentage_3mo,
+        "relative_high_percentage_6mo": relative_high_percentage_6mo,
+        "relative_high_percentage_1yr": relative_high_percentage_1yr,
+        "relative_mean_percentage_3mo": relative_mean_percentage_3mo,
+        "relative_mean_percentage_6mo": relative_mean_percentage_6mo,
+        "relative_mean_percentage_1yr": relative_mean_percentage_1yr,
+    }
 
 
-msg = "\nToday stock price:\n\n"
+msg = "\nToday stock relative price:\n\n"
 
 for stock in STOCK_LIST:
     try:
-        price, percentage = get_stock_info(stock)
-        msg += f"{stock}: Now {price:.2f}, relative high {percentage:.2f}%\n"
+        stock_info = get_stock_info(stock)
+        msg += f"{stock}: Now {stock_info['current_price']:.2f}\n"
+        msg += f"  [3mo] high {stock_info.get('relative_high_percentage_3mo', 'NaN'):.1f}%, mean {stock_info.get('relative_mean_percentage_3mo', 'NaN'):.1f}%\n"
+        msg += f"  [6mo] high {stock_info.get('relative_high_percentage_6mo', 'NaN'):.1f}%, mean {stock_info.get('relative_mean_percentage_6mo', 'NaN'):.1f}%\n"
+        msg += f"  [1yr] high {stock_info.get('relative_high_percentage_1yr', 'NaN'):.1f}%, mean {stock_info.get('relative_mean_percentage_1yr', 'NaN'):.1f}%\n"
     except Exception as e:
         print(f"Fetching stock {stock} error: {e}")
         msg += f"{stock}: Unable to fetch data\n"
