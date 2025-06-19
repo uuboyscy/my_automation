b48c3fed1f29bec3d29909c0e0d4ef2e64376647
uuboyscy <aegis12321@gmail.com>
2025-06-19 15:16:08 +0800

    feat(geekbench-report): :sparkles: Add function to retrieve CPU model names from PostgreSQL database

--- src/utils/geekbench_report/database_helper.py
+++ src/utils/geekbench_report/database_helper.py
@@ -16,6 +16,28 @@ GEEKBENCH_REPORT_POSTGRESDB_USER = os.getenv("GEEKBENCH_REPORT_POSTGRESDB_USER")
 GEEKBENCH_REPORT_POSTGRESDB_PASSWORD = os.getenv("GEEKBENCH_REPORT_POSTGRESDB_PASSWORD")
 
 
+def get_cpu_model_name_list_from_pg(limit: int | None = None) -> list[str]:
+    if not limit:
+        limit = 1
+
+    with get_postgresql_conn(
+        database=GEEKBENCH_REPORT_POSTGRESDB_DATABASE,
+        user=GEEKBENCH_REPORT_POSTGRESDB_USER,
+        password=GEEKBENCH_REPORT_POSTGRESDB_PASSWORD,
+        host=GEEKBENCH_REPORT_POSTGRESDB_HOST,
+        port=GEEKBENCH_REPORT_POSTGRESDB_PORT,
+    ) as conn:
+        sql = "select cpu_model FROM cpu_model_names"
+
+        if limit is not None:
+            sql += f" limit {limit}"
+
+        return pd.read_sql(
+            sql,
+            conn,
+        )["cpu_model"].to_list()
+
+
 def get_last_updated_dates_of_cpu_model_df() -> pd.DataFrame:
     sql = """
     """
