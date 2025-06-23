475f0ad9e2d6a0789b2d78faaef30b6bc3252583
uuboyscy <aegis12321@gmail.com>
2025-06-23 16:18:03 +0800

    feat(geekbench-report): :sparkles: Enhance sync_cpu_model_result_to_pg function to update CPU and system names, and streamline data loading to PostgreSQL

--- src/app/geekbench_report/sync_cpu_model_result_to_pg.py
+++ src/app/geekbench_report/sync_cpu_model_result_to_pg.py
@@ -4,7 +4,13 @@ from utils.geekbench_report.core.geekbench_processor_result_scraper import (
     GeekbenchProcessorResultScraper,
 )
 from utils.geekbench_report.database_helper import (
+    delete_cpu_model_result_record_from_date_to_now,
+    get_cpu_model_map_from_pg,
     get_last_updated_dates_of_cpu_model_df,
+    get_system_map_from_pg,
+    load_df_to_pg,
+    update_cpu_model_names,
+    update_system_names,
 )
 
 
@@ -20,9 +26,11 @@ def sync_cpu_model_result_to_pg() -> None:
 
         start_time = time.time()
 
-        # delete_cpu_model_result_record_from_date_to_now(
-        #     cpu_model=cpu_model_name, from_date=last_updated_date,
-        # )
+        delete_cpu_model_result_record_from_date_to_now(
+            cpu_model=cpu_model_name,
+            from_date=last_updated_date,
+        )
+
         scraper = GeekbenchProcessorResultScraper(
             cpu_model_name,
             offset_date=last_updated_date,
@@ -33,8 +41,21 @@ def sync_cpu_model_result_to_pg() -> None:
 
         df = scraper.scrape_multiple_pages_until_offset_date()
 
-        print(df)
-        # load_df_to_pg(df=df, table_name="cpu_model_results", if_exists="append")
+        # update system_names if new system detected
+        update_system_names(df["system"])
+        # system -> system_id
+        df["system_id"] = df["system"].map(get_system_map_from_pg())
+        # update cpu_model_names if new cpu_model detected
+        update_cpu_model_names(df["cpu_model"])
+        # cpu_model -> cpu_model_id
+        df["cpu_model_id"] = df["cpu_model"].map(get_cpu_model_map_from_pg())
+
+        print(df.drop(["system", "cpu_model"], axis=1))
+        load_df_to_pg(
+            df=df.drop(["system", "cpu_model"], axis=1),
+            table_name="cpu_model_results",
+            if_exists="append",
+        )
 
         print(f"Total results found: {len(df)}")
         print(f"{time.time() - start_time} seconds took.")
