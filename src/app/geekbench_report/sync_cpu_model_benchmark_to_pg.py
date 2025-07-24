ef3abab91ac0fd798b97cb5dd0964bb6fec5f1c1
uuboyscy <aegis12321@gmail.com>
2025-07-24 15:31:52 +0800

    docs(geekbench-report): :memo: Update docstring to clarify source and target for syncing CPU model benchmarks

--- src/app/geekbench_report/sync_cpu_model_benchmark_to_pg.py
+++ src/app/geekbench_report/sync_cpu_model_benchmark_to_pg.py
@@ -1,10 +1,33 @@
+"""
+Sync CPU model record on benchmark page to PostgreSQL.
+
+Source:
+    - Webpage
+        - https://browser.geekbench.com/processor-benchmarks
+Target:
+    - PostgreSQL
+        - geekbench_report.cpu_model_benchmarks
+            ```sql
+            -- Table Definition
+            CREATE TABLE "public"."cpu_model_benchmarks" (
+                "cpu_model" text,
+                "frequency" text,
+                "cores" int8,
+                "single_core_score" int8,
+                "multi_core_score" int8
+            );
+            ```
+
+Run in n8n container:
+Not scheduled yet.
+
+"""
+
 from dataclasses import asdict
 
 import pandas as pd
 
-from utils.geekbench_report.core.geekbench_processor_benchmark_scraper import (
-    scrape_page,
-)
+from utils.geekbench_report.core.geekbench_processor_benchmark_scraper import scrape_page
 from utils.geekbench_report.database_helper import load_df_to_pg
 
 
